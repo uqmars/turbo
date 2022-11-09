ba68952ebf199003dd0e3c3f72bb8e9e87720fbe
ThatSealgair <sealgair@X1-Windows.localdomain>
2022-11-09 22:22:01 +1000

    Turbo now pattern matches for a help button

---
+++ src/banter.rs
@@ -0,0 +1,16 @@
+const REPLY_1: &str = "
+Bant her? I only just met her!
+";
+
+const REPLY_2: &str = "
+I hardly know her!
+";
+
+const REPLY_3: &str = "
+Only if she'll let ya!
+";
+
+const REPLY_4: &str = "
+You may say, it is impossible for a man to become like the Machine. And I would reply, that only the smallest mind strives to comprehend its limits.
+";
+
