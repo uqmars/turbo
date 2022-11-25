2b3ac047cbfd94afc6b8ba291fda6f3d9f2cfbf0
ThatSealgair <sealgair@X1-Windows.localdomain>
2022-11-26 09:29:17 +1000

    Added test image and modified the AOC.rs code

--- src/advent-of-code.rs
+++ src/advent-of-code.rs
@@ -5,8 +5,16 @@ use pyo3::{prelude::*, types::{IntoPyDict, PyModule}};
 ///
 /// Activates the Advent of Code Python Script
 
-pub async fn aoc() {
-    Python::with_gil(|py| {
-        let aoc_help = PyModule::from_code(py, #r"run", "/python-scripts/aoc/main.py", "help")?;
-    })
-}
+const AOC_SCRIPT: &str = "/python-scripts/aoc/main.py".to_string();
+
+pub async fn aoc(ctx: CTX, msg: Message) -> String {
+    let aoc_help = PyModule::from_code(py, #r"
+        async def help(ctx, *args):
+        ", AOC_SCRIPT, "help")?;
+
+    let aoc-help-message: String = aoc_help.getattr("help")?
+        .call(msg.to_string())?
+        .extract()?;
+    
+    return aoc-help-message.to_string();
+ }
